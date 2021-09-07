035386a5bcf7e43527097d22578b46ab8a6517a9
Mrityunjay Kumar <geekmj@users.noreply.github.com>
2021-09-07 04:01:14 +0000

    some great changes

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,12 @@
 public class HelloWorld {
   public static void main(String args[]) {
-    System.out.println("Hello World");
+    System.out.println("Factorial 10 " + factorial(10));
+  }
+
+  public static int factorial(int n) {
+    if(n <=0){
+      return 0;
+    }
+    return n + factorial(n-1);
   }
 }
